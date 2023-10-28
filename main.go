b17dceff211ca4018d58f7748eb45e8f03edc0b8
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2023-10-28 12:28:07 +0530

    feat adding rest api functionality

--- main.go
+++ main.go
@@ -5,48 +5,79 @@ import (
   "fmt"
   "net/smtp"
   "text/template"
+  "github.com/gin-gonic/gin"
+  "net/http"
 )
 
+type EmailRequest struct {
+  FromEmail    string `json:"fromEmail"`
+  FromPassword string `json:"fromPassword"`
+  ToEmail      string `json:"toEmail"`
+  Subject string `json:"subject"`
+  Body    string `json:"body"`
+}
 
+
+
+
+func getEmailRequest(c *gin.Context)  {
+  email := EmailRequest{
+    FromEmail:    c.Query("email"),
+    FromPassword: c.Query("password"),
+    ToEmail:      c.Query("to"),
+    Subject: c.Query("subject"),
+    Body:    c.Query("body"),
+  }
+  var temp = sendEmail(email);
+  // Print the query parameters.
+  // fmt.Println("name and age is: ",name, age)
+  c.IndentedJSON(http.StatusOK, temp)
+}
 
 func main() {
+  router := gin.Default()
+  router.GET("/sendEmail", getEmailRequest)
+  router.Run("localhost:8080")
+}
 
-  // Sender data.
-  from := "<email-address>"
-  password := "<password>"
-
-  // Receiver email address.
-  to := []string{
-    "<receiver-email-address>",
-  }
-
-  // smtp server configuration.
-  smtpHost := "smtp.gmail.com"
-  smtpPort := "587"
-
-  // Authentication.
-  auth := smtp.PlainAuth("", from, password, smtpHost)
-
-  t, _ := template.ParseFiles("template.html")
-
-  var body bytes.Buffer
-
-  mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-  body.Write([]byte(fmt.Sprintf("Subject: This is a test subject \n%s\n\n", mimeHeaders)))
-
-  t.Execute(&body, struct {
-    Name    string
-    Message string
-  }{
-    Name:    "name",
-    Message: "message",
-  })
-
-  // Sending email.
-  err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fmt.Println("Email Sent!")
+func sendEmail(data EmailRequest) string {
+    // Sender data.
+    from := data.FromEmail
+    password := data.FromPassword
+  
+    // Receiver email address.
+    to := []string{
+      data.ToEmail,
+    }
+  
+    // smtp server configuration.
+    smtpHost := "smtp.gmail.com"
+    smtpPort := "587"
+  
+    // Authentication.
+    auth := smtp.PlainAuth("", from, password, smtpHost)
+  
+    t, _ := template.ParseFiles("template.html")
+  
+    var body bytes.Buffer
+  
+    mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+    body.Write([]byte(fmt.Sprintf("Subject:"+data.Subject+" \n%s\n\n", mimeHeaders)))
+  
+    t.Execute(&body, struct {
+      Name    string
+      Message string
+    }{
+      Name:    "name",
+      Message: "message",
+    })
+  
+    // Sending email.
+    err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
+    if err != nil {
+      fmt.Println(err)
+      return "Email Not Sent!, Error!: " + err.Error();
+    }
+    fmt.Println("Email Sent!")
+    return "Email Sent!";
 }
